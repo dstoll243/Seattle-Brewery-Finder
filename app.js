2a65a2e8fade3fb9353501539a695e32ca19ece6
Tyler <Tconfalone@gmail.com>
2018-02-24 10:37:39 -0800

    working on js

--- app.js
+++ app.js
@@ -32,4 +32,17 @@ function makeBrews() {
 makeBrews();
 console.log(allBrews);
 
+function() {
+    var questions = [{
+        question: "Are you looking for a brewery that allows children?",
+        choices: [yes,no],
+        kidAnswer: 2
+    }, {
+        question: "Are you looking for a brewery that allows pets?",
+        choices: [yes, no],
+        petAnswer: 7
+    }]
+};
+
+
 //push to local storage
