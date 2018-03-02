f68a938d502c2b4433187f02dbf8510dea797821
Tyler <Tconfalone@gmail.com>
2018-03-01 19:08:52 -0800

    about me words

--- app.js
+++ app.js
@@ -123,7 +123,6 @@ var ageCheck = false;
 var older = document.getElementById('older');
 var younger = document.getElementById('young');
 var popUp = document.getElementById('popup');
-var ofAge = localStorage.getItem("ageCheck");
 
 //hide popup if 21 and older
 function showPop() {
